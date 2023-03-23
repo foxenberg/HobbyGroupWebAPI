e8211d31f3936ca86899af8568dc0273e02d21ac
foxenberg <daniel.korshunov.2001@gmail.com>
2023-03-23 17:53:06 +0300

    Added controllers and views

---
+++ WebPortal/Controllers/ClubController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebPortal.Data;
+using WebPortal.Models;
+
+namespace WebPortal.Controllers
+{
+    public class ClubController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public ClubController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            List<Club> clubs = _context.Clubs.ToList();
+            return View(clubs);
+        }
+
+        public IActionResult Detail(int id)
+        {
+            Club club = _context.Clubs.FirstOrDefault(c => c.Id == id);
+            return View(club);
+        }
+    }
+}
---
+++ WebPortal/Controllers/RaceController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebPortal.Data;
+using WebPortal.Models;
+
+namespace WebPortal.Controllers
+{
+    public class RaceController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public RaceController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            List<Race> races = _context.Races.ToList();
+            return View(races);
+        }
+        public IActionResult Detail(int id)
+        {
+            Race race = _context.Races.FirstOrDefault(c => c.Id == id);
+            return View(race);
+        }
+    }
+}
